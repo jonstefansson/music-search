4c22ca71e26a26b690b255ddfa7200bbcf7b8306
Jon Stefansson <jonstef1667@gmail.com>
2015-05-18 15:15:33 -0700

    Improvement to aggregation.sh query and jq filter to reduce output to sorted list of albums

--- curl/aggregation.sh
+++ curl/aggregation.sh
@@ -8,5 +8,4 @@ curl \
 --header "Content-Type: application/json; charset=UTF-8" \
 --header "Accept: application/json" \
 --data @- \
---url http://localhost:9200/music/track/_search?search_type=count \
-| jq '.'
+--url http://localhost:9200/music/track/_search?search_type=count
